14b3947dcdc57b630d34935bd251a7c8e42b0412
liwenqiang <zooeymoon1989@gmail.com>
2023-07-13 03:49:44 +0800

    add gitignore

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
@@ -15,8 +16,11 @@ func main() {
 		host := c.Request.Host
 		c.String(http.StatusOK, fmt.Sprintf("hello from go-gin , the host address is: %s, and the hostname is: %s", host, id))
 	})
+
+	r.GET("/metrics", gin.WrapH(promhttp.Handler()))
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
 	})
-	r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":80") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
