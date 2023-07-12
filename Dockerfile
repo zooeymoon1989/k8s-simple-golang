24ba6078be3c8f65cc95601c3d187299804acbad
liwenqiang <zooeymoon1989@gmail.com>
2023-07-13 03:41:10 +0800

    update go mod and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -18,6 +18,6 @@ FROM alpine
 
 COPY --from=builder /app/server /app/server
 
-EXPOSE 3000
+EXPOSE 8080
 
 ENTRYPOINT ["/app/server"]
--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"net/http"
 	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -17,5 +18,5 @@ func main() {
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
 	})
-	r.Run(":3000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
